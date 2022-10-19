46906ae97fa1d432ea00e3effb46c6541783f059
bmafi@melo <fm.melo@campus.fct.unl.pt>
2022-10-19 11:57:55 +0100

    fixing repo

--- lab2/messages/src/main/kotlin/pt/unl/fct/di/iadi/messages/presentation/MessageDAO.kt
+++ lab2/messages/src/main/kotlin/pt/unl/fct/di/iadi/messages/presentation/MessageDAO.kt
@@ -3,5 +3,5 @@ package pt.unl.fct.di.iadi.messages.presentation
 import javax.persistence.Entity
 import javax.persistence.Id
 
-@Entity
-data class MessageDAO(@Id val to:String, val cc:String, val subject:String, val body:String)
+// @Entity
+//data class MessageDAO(@Id val to:String, val cc:String, val subject:String, val body:String)
